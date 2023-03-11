f212e8573857ca7d6c06d75c2b37e262fd926e4f
lothar <lothar>
2023-03-11 10:19:03 +0000

    Moved some files from TVVT to here to have all together. Still some stuff in other folders

---
+++ TVDV/helpcmds.pas
@@ -0,0 +1,44 @@
+unit Helpcmds;
+
+interface
+
+const
+
+  hcAboutHelp            = 50;
+  hcAufreum              = 803;
+  hcBeenden              = 1004;
+  hcCDROM                = 1008;
+  hcChangeDir            = 808;
+  hcContents             = 1000;
+  hcCursor               = 819;
+  hcDAS                  = 801;
+  hcDatei                = 815;
+  hcDateileichen         = 803;
+  hcDaten                = 817;
+  hcDatenAkt             = 816;
+  hcDatenAufgeben        = 806;
+  hcDEdit                = 813;
+  hcEditor               = 820;
+  hcEinstellungen        = 818;
+  hcHotKey               = 1006;
+  hcImport               = 821;
+  hcImportieren          = 1001;
+  hcImportNachtragen     = 1003;
+  hcImportPr             = 1002;
+  hcLaden                = 804;
+  hcMaus                 = 1007;
+  hcMemory               = 812;
+  hcMenuAkt              = 1005;
+  hcNachtrag             = 802;
+  hcNewFileName          = 807;
+  hcNoContext            = 0;
+  hcPruefen              = 814;
+  hcScann                = 800;
+  hcSpeichern            = 805;
+  hcSucheDaten           = 810;
+  hcSucheMehrfache       = 811;
+  hcZeigeDaten           = 809;
+
+implementation
+
+end.
